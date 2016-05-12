c07f2983a8aaec5c02fc4487a8d07263a65a2698
annihibert <ahibertjr@gmail.com>
2016-05-12 11:39:25 -0500

    resolvi greedy

---
+++ hacker1/credit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	
+	long creditno;
+
+	printf ("Enter a valid credit card number (no hyphens)\n");
+	scanf ("%ld", &creditno);
+	printf ("Your credit card no is %ld\n", creditno);
+
+
+
+}
